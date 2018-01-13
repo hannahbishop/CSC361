c21fa9b2b96be125b419de71c14b83150df2bb0e
Hannah <hannahbananabishop@gmail.com>
2018-01-13 12:50:38 -0800

    add SSL test

--- Assignments/A1/A1.py
+++ Assignments/A1/A1.py
@@ -6,15 +6,30 @@ parser = argparse.ArgumentParser(description='Query a server.')
 parser.add_argument('host', type=str, help='a URL to query')
 args = parser.parse_args()
 
-def main():
+#create SSL socket
+def createSSL():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     context = ssl.create_default_context()
-    conn = context.wrap_socket(sock, server_hostname="www.uvic.ca")
+    conn = context.wrap_socket(sock, server_hostname=args.host)
+    return conn
+
+#create regular socket
+def createSock():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    return sock
+
+def main():
     try:
-        conn.connect(("www.uvic.ca", 443))
-        conn.sendall(b"HEAD / HTTP/1.1\r\nHost: www.uvic.ca\r\n\r\n")
+        conn = connectSSL()
+        conn.connect((args.host, 443))
+        conn.sendall(b"HEAD / HTTP/1.1\r\nHost: " + args.host + "\r\n\r\n")
         pprint.pprint(conn.recv(1024).split(b"\r\n"))
-    finally:
         conn.close()
+    except:
+        sock = createSock()
+        sock.connect((args.host, 80))
+        sock.sendall(b"HEAD / HTTP/1.1\r\nHost: " + args.host + "\r\n\r\n")
+        pprint.pprint(sock.recv(1024).split(b"\r\n"))
+        sock.close()
 
 main()
