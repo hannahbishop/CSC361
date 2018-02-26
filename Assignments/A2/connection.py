9c86f03658f518e4713e85e216de6672e584c902
Hannah <hannahbananabishop@gmail.com>
2018-02-26 09:44:31 -0800

    increment syn and fin

--- Assignments/A2/connection.py
+++ Assignments/A2/connection.py
@@ -5,6 +5,8 @@ class _Connection():
         self.dest_addr = dest_addr
         self.dport = dport
         self.flags = flags
+        self.syn = 0
+        self.fin = 0
 
     def __eq__(self, other):
         equal = True
@@ -25,6 +27,25 @@ class _Connection():
         else:
             return False
 
+    def __repr__(self):
+        return "Item(%s, %s)" % (self.foo, self.bar)
+
     def print(self):
         print(self.src_addr, self.sport, self.dest_addr, self.dport, self.flags)
+        print(self.syn, self.fin)
+        return
+    
+    def inc_syn(self):
+        if self.syn == 2:
+            print("Cannot increment syn")
+            return -1
+        self.syn += 1
+        return
+
+    def inc_fin(self):
+        if self.fin == 2:
+            print("Cannot increment fin")
+            return -1
+        self.fin += 1
+        return
     
--- Assignments/A2/tcp.py
+++ Assignments/A2/tcp.py
@@ -15,11 +15,23 @@ def packet_loop(fp):
         eth = dpkt.ethernet.Ethernet(buf)
         ip = eth.data
         tcp = ip.data
-        conn = _Connection(ip.src, tcp.sport, ip.dst, tcp.dport, format(tcp.flags, '06b'))
+        flags = format(tcp.flags, '08b')
+        conn = _Connection(ip.src, tcp.sport, ip.dst, tcp.dport, flags)
+        #messy, fix this. can I just edit in place? this looks terrible
         if conn in connections:
-            continue
+            for i in range(len(connections)):
+                if connections[i] == conn:
+                    #increment syn and fin if they are set
+                    if (tcp.flags & 0b00000010) == 2:
+                        print("inc syn")
+                        connections[i].inc_syn()
+                    if (tcp.flags & 0b00000001) == 1:
+                        print("inc fin")
+                        connections[i].inc_fin()
         else:
             connections.append(conn)
+    for conn in connections:
+        conn.print()
     return
 
 
