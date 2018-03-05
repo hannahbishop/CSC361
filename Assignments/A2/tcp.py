1318956ee2eeeef13537777dc2e64aaafa930ea0
Hannah Bishop <hannahbananabishop@gmail.com>
2018-03-04 21:24:23 -0800

    refactor

--- Assignments/A2/tcp.py
+++ Assignments/A2/tcp.py
@@ -38,56 +38,71 @@ def add_connections(fp):
             connections[i].add_rtt(ts - rtt[tcp.ack])
     return connections
 
+def durations(connections):
+    durations = []
+    for conn in connections:
+        durations.append(conn.get_duration())
+    return min(durations), (sum(durations)/len(connections)), max(durations)
+
+def wins(connections):
+    wins = []
+    for conn in connections:
+        wins += conn.get_win()
+    return min(wins), (sum(wins)/len(connections)), max(wins)
+
+def rtts(connections):
+    rtts = []
+    for conn in connections:
+        rtts += conn.get_rtt()
+    return min(rtts), (sum(rtts)/len(rtts)), max(rtts)
+
+def packets(connections):
+    packets = []
+    for conn in connections:
+        packets.append(conn.get_num_packets())
+    return min(packets), (sum(packets)/len(connections)), max(packets)
+
 def analyze_connections(connections):
     num_connections = 0
-    complete = 0
-    incomplete = 0
+    num_complete = 0
     reset = 0
-    min_duration = min_packets = min_win = 99999999999
-    max_duration = max_packets = max_win = 0
-    total_duration = total_packets = total_win = 0
-
-    durations = []
-    rtt = []
-    packets = []
-    win = []
+    complete = []
 
     for i, conn in enumerate(connections):
         num_connections += 1
         print("Connection {}\n".format(i + 1))
         conn.print_data()
         if conn.is_complete():
-            complete += 1
-            durations.append(conn.get_duration())
-            packets.append(conn.get_num_packets())
-            win += conn.get_win()
-            rtt += conn.get_rtt()
-        else:
-            incomplete += 1
+            complete.append(conn)
+            num_complete += 1
         if conn.get_rst():
             reset += 1
         print("\n------------------------------\n")
+    min_duration, mean_duration, max_duration = durations(complete)
+    min_win, mean_win, max_win = wins(complete)
+    min_rtt, mean_rtt, max_rtt = rtts(complete)
+    min_packet, mean_packet, max_packet = packets(complete)
     print("Number of TCP Connections: {}".format(num_connections))
     print("Number of reset TCP connections observed in the trace: {}".format(reset))
-    print("Number of TCP connections that were still open when the trace capture ended: {}".format(incomplete))
-    print("Number of complete TCP connections observed in the trace: {}".format(complete))
+    print("Number of TCP connections that were still open when the trace capture ended: {}".format(num_connections - num_complete))
+    print("Number of complete TCP connections observed in the trace: {}".format(num_complete))
     print("\n------------------------------\n")
     print("Complete Connections:\n")
-    print("Minimum Time Duration: %.5f" % min(durations))
-    print("Mean Time Duration: %.5f" % (sum(durations)/complete))
-    print("Maximum Time Duration: %.5f\n" % max(durations))
+    print("Minimum Time Duration: %.5f" % min_duration)
+    print("Mean Time Duration: %.5f" % mean_duration)
+    print("Maximum Time Duration: %.5f\n" % max_duration)
 
-    print("Minimum Packets (both directions): ", min(packets))
-    print("Mean Packets (both directions): ", sum(packets)/complete)
-    print("Maximum Packets (both directions): ", max(packets), "\n")
+    print("Minimum Packets (both directions): ", min_packet)
+    print("Mean Packets (both directions): ", mean_packet)
+    print("Maximum Packets (both directions): ", max_packet, "\n")
     
-    print("Minimum receive window size (both directions): ", min(win))
-    print("Mean receive window size (both directions): %.4f" % (sum(win)/complete))
-    print("Maximum receive window size (both directions): ", max(win), "\n")
+    print("Minimum receive window size (both directions): ", min_win)
+    print("Mean receive window size (both directions): %.4f" % mean_win)
+    print("Maximum receive window size (both directions): ", max_win, "\n")
 
-    print("Minimum RTT value: ", min(rtt))
-    print("Mean RTT value: ", sum(rtt)/len(rtt))
-    print("Maximum RTT value: ", max(rtt))
+    print("Minimum RTT value: ", min_rtt)
+    print("Mean RTT value: ", mean_rtt)
+    print("Maximum RTT value: ", max_rtt)
     
     return
 
