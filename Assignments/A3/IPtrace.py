be26e587e4f1f83d232d4d71c27e56cf3a63ad52
Hannah Bishop <hannahbananabishop@gmail.com>
2018-04-01 08:41:06 -0700

    breadcrumb

--- Assignments/A3/IPtrace.py
+++ Assignments/A3/IPtrace.py
@@ -9,6 +9,7 @@ from ICMP import _ICMP
 def extract_datagrams(pcap) -> (list, list, set):
     (incoming, outgoing) = ([], [])
     protocols = set()
+    (linux, win) = (False, False)
     for ts, buf in pcap:
 
         eth = dpkt.ethernet.Ethernet(buf)
@@ -21,16 +22,22 @@ def extract_datagrams(pcap) -> (list, list, set):
         ip_dst = socket.inet_ntoa(ip.dst)
         #UDP
         if ip.p == 17 and ip.ttl == len(outgoing) + 1:
+            linux = True
             udp = _UDP(ip_src, ip_dst, ts, ip.ttl, ip.p, ip.data.sport, ip.data.dport)
             outgoing.append(udp)
         #ICMP
         if ip.p == 1:
             icmp_type = ip.data.type
             if icmp_type in (0, 8) and ip.ttl == len(outgoing) + 1:
+                win = True
                 seq = ip.data.data.seq
                 icmp = _ICMP(ip_src, ip_dst, ts, ip.ttl, ip.p, seq)
                 outgoing.append(icmp)
-            if icmp_type == 11:
+            if icmp_type == 11 and win:
+                seq = ip.data.data.data.data['echo'].seq
+                icmp = _ICMP(ip_src, ip_dst, ts, ip.ttl, ip.p, seq)
+                incoming.append(icmp)
+            if icmp_type == 11 and linux:
                 seq = ip.data.data.data.data['echo'].seq
                 icmp = _ICMP(ip_src, ip_dst, ts, ip.ttl, ip.p, seq)
                 incoming.append(icmp)
@@ -40,6 +47,9 @@ def extract_datagrams(pcap) -> (list, list, set):
 
 def find_path(incoming: list, outgoing: list) -> list:
     path = []
+    #add the first
+    path.append(outgoing[0].src)
+    #add the intermediates
     for i, out in enumerate(outgoing):
         #UDP - match ports
         if out.p == 17:
@@ -49,15 +59,23 @@ def find_path(incoming: list, outgoing: list) -> list:
             resp = [resp for resp in incoming if resp.seq == out.seq]
             if resp:
                 path.append(resp[0].src)
+    #add the last
+    path.append(outgoing[0].dst)
     return path
         
+def print_info(path):
+    print("Source Node: ", path[0])
+    print("Ultimate Destination Node: ", path[-1])
+    print("Intermediate Nodes:")
+    for i, ip in enumerate(path[1:-1]):
+        print("  ", ip)
+
 def main():
-    with open("win_trace1.pcap", "rb") as fp:
+    with open("trace1.pcap", "rb") as fp:
         pcap = dpkt.pcap.Reader(fp)
         (incoming, outgoing, protocols) = extract_datagrams(pcap)
         path = find_path(incoming, outgoing)
-        for ip in path:
-            print(ip)
+        print_info(path)
 
 if __name__ == "__main__":
     main()
